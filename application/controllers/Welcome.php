7330e5d1c6dff877c2f0cb5fdb18c2e8d2903629
Bradley Slavens <Bradley Slavens>
2016-03-17 15:43:58 -0700

    updated welcome

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -30,33 +30,50 @@ class Welcome extends CI_Controller {
 
 	public function index()
 	{
+		/**
+		 * load the form helper
+		 */
 		$this->load->helper('form');
 
+		/**
+		 * load the header , includes bootstrap etc.
+		 */
 		$this->load->view('templates/header');
-		$this->load->view('welcome_message2');
 
-		//PROCESS registration form
+		/**
+		 * Validate login
+		 */
 		$this->load->library('form_validation');
 
+		/**
+		 * Set vaildation rules
+		 */
+		$this->form_validation->set_rules('email', 'email', 'required|xss_clean');
+		$this->form_validation->set_rules('password', 'Password', 'required | xss_clean');
 
-		// // validate form
-		// $this->form_validation->set_rules('first_name', 'First Name', 'required');
-		// $this->form_validation->set_rules('last_name', 'Last Name', 'required');
-		// $this->form_validation->set_rules('email', 'email', 'required');
-		// $this->form_validation->set_rules('password', 'Password', 'required');
-		// $this->form_validation->set_rules('first_name', 'First Name', 'required');
 
-		// if($this->form_validation->run()===FALSE)
-		// {
+		/**
+		 * Check validation
+		 */
+		if($this->form_validation->run()===FALSE)
+		{
+			/**
+			 * If the login fails validation
+			 * load the welcome page
+			 */
+			$this->load->view('welcome_message');
+		}
+		else
+		{
+			/**
+			 * If the login succeeds set the user 
+			 */
+			$data['users']=$this->user_model->set_user();
+		}
 
-		// 	$this->load->view('registration');
-
-		// }
-		// else
-		// {
-		// 	$data['users']=$this->user_model->set_user();
-		// }
-
+		/**
+		 * Load the footer , includes scripts
+		 */
 		$this->load->view('templates/footer');
 
 	}
--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,55 +1,51 @@
+<section id="login" class="container">
+    <h1 id="greeting">Welcome to On-It Transactions</h1>
+    <!-- Button trigger modal -->
+    <button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+      Login
+    </button>
 
-<nav class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container">
-        <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">TC Slavens</a>
+    <!-- Modal -->
+    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <h4 class="modal-title" id="myModalLabel">Login</h4>
+          </div>
+          <div class="modal-body">
+             <?php echo form_open('welcome/check_login'); ?>
+
+              <span style="color:white;"><?php echo validation_errors(); ?></span>
+              
+              <div class="form-group">
+                <?php 
+                  // set params for input
+                  $data = array(
+                        'class'=>'form-control',
+                        'placeholder'=>'Email',
+                        'name'=>'email',
+                        'type'=>'email');
+                        ?>
+                <?php echo form_input($data);
+                //should echo <input type="text" placeholder="Email" class="form-control">
+                ?>
+              </div>
+              <div class="form-group">
+                <?php
+                  $data = array (
+                      'placeholder'=>'Password',
+                      'class' => 'form-control',
+                      'name'=>'password');
+                  echo form_password($data); 
+                //<input type="password" placeholder="Password" class="form-control">
+                ?>
+              </div>
+              <button type="submit" name="submit" class="btn btn-success">Sign in</button>
+            </form>
+          </div>
         </div>
-        <div id="navbar" class="navbar-collapse collapse">
-
-              <?php 
-                // set params for form
-                $data = array(
-                      'class'=>'navbar-form navbar-right'
-                      );
-                      ?>
-          <?php echo form_open('welcome/check_login', $data); ?>
-
-            <span style="color:white;"><?php echo validation_errors(); ?></span>
-            
-            <div class="form-group">
-              <?php 
-                // set params for input
-                $data = array(
-                      'class'=>'form-control',
-                      'placeholder'=>'Email',
-                      'name'=>'email',
-                      'type'=>'email');
-                      ?>
-              <?php echo form_input($data);
-              //should echo <input type="text" placeholder="Email" class="form-control">
-              ?>
-            </div>
-            <div class="form-group">
-              <?php
-                $data = array (
-                    'placeholder'=>'Password',
-                    'class' => 'form-control',
-                    'name'=>'password');
-                echo form_password($data); 
-              //<input type="password" placeholder="Password" class="form-control">
-              ?>
-            </div>
-            <button type="submit" name="submit" class="btn btn-success">Sign in</button>
-          </form>
-        </div><!--/.navbar-collapse -->
       </div>
-    </nav>
-
-    <!-- Main jumbotron for a primary marketing message or call to action -->
-   
+    </div> <!-- modal -->
+    
+</section> <!-- container -->
